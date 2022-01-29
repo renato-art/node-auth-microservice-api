bee4a38f6ee7460aff9dfd3a37a9801da98f763b
renato <renato.oliveira83@outlook.com>
2022-01-29 18:22:25 -0300

    ensure LoginRouter return 500 if EmailValidator throws

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -26,6 +26,15 @@ const makeEmailValidator = () => {
     return emailValidatorSpy
 }
 
+const makeEmailValidatorWithError = () => {
+    class EmailValidatorSpy {
+        isValid (email) {
+            throw new Error()
+        }
+    }
+    return new EmailValidatorSpy()
+}
+
 const makeAuthUseCase = () => {
     class AuthUseCaseSpy {
        async auth (email, password) {
@@ -186,6 +195,20 @@ describe('Login Router', () => {
     test('Should return 500 if EmailValidator has no isValid method', async () => {
         const authUseCaseSpy = makeAuthUseCase()
         const sut = new LoginRouter(authUseCaseSpy, {})
+        const httpRequest = {
+            body: {
+                email: 'any_email@mail.com',
+                password: 'any_password'
+            }
+        }
+        const httpResponse = await sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(500)
+        expect(httpResponse.body).toEqual(new ServerError())
+    })
+    test('Should return 500 if EmailValidator throws', async () => {
+        const authUseCaseSpy = makeAuthUseCase()
+        const emailValidatorSpy = makeEmailValidatorWithError()
+        const sut = new LoginRouter(authUseCaseSpy, emailValidatorSpy)
         const httpRequest = {
             body: {
                 email: 'any_email@mail.com',
