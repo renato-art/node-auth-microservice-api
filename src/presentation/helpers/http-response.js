c4095d6d7f70872475f70c4a8ad8f795076430fa
renato <renato.oliveira83@outlook.com>
2022-01-29 21:10:59 -0300

    move errors to a custom folder

--- src/presentation/helpers/http-response.js
+++ src/presentation/helpers/http-response.js
@@ -1,30 +1,31 @@
-const MissingParamError = require('./missing-param-error')
-const UnauthorizedError = require('./unauthorized-error')
-const ServerError = require('./server-error')
+const { UnauthorizedError, ServerError } = require('../errors')
 
 module.exports = class HttpResponse {
-    static badRequest (error) {
-        return {
-            statusCode: 400,
-            body: error
-        }
+  static badRequest (error) {
+    return {
+      statusCode: 400,
+      body: error
     }
-    static serverError () {
-        return {
-            statusCode: 500,
-            body: new ServerError()
-        }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500,
+      body: new ServerError()
     }
-    static unauthorizedError () {
-        return {
-            statusCode: 401,
-            body: new UnauthorizedError()
-        }
+  }
+
+  static unauthorizedError () {
+    return {
+      statusCode: 401,
+      body: new UnauthorizedError()
     }
-    static ok (data) {
-        return {
-            statusCode: 200,
-            body: data
-        }
+  }
+
+  static ok (data) {
+    return {
+      statusCode: 200,
+      body: data
     }
+  }
 }
